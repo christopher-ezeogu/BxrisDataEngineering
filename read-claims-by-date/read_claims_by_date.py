b241d47743626f9a3e565fcf6ddff9345d812378
christopher-ezeogu <eze.chris444@gmail.com>
2026-03-26 14:42:02 -0400

    updated code

--- read-claims-by-date/read_claims_by_date.py
+++ read-claims-by-date/read_claims_by_date.py
@@ -13,7 +13,13 @@ df["service_date"] = pd.to_datetime(df["service_date"], errors="coerce")
 # remove missing dates
 valid = df.dropna(subset=REQUIRED_FIELD)
 
-# filter records from 2025
-records_from_2025 = valid[valid["service_date"].dt.year == 2025]
+# filter records by year, month and day -- good when searching for individually
+records_from_2025 = valid[
+        (valid["service_date"].dt.year == 2025) &
+        (valid["service_date"].dt.month == 3) & 
+        (valid["service_date"].dt.day == 28)
+    ]
+# best when searching for entire date - cleaner and faster
+#records = valid[valid["service_date"] == pd.Timestamp("2025-03-15")]
 
 print(records_from_2025.head(10))
